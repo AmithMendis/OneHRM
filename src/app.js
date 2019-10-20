c826bb884d697f809b12d2e856a3a4362fb7d4c4
AmithMendis <amith.mendis@gmail.com>
2019-10-20 18:26:44 +0530

    graphql added

--- src/app.js
+++ src/app.js
@@ -2,12 +2,17 @@ const express = require('express');
 const bodyParsor = require('body-parser');
 
 const app = express();
-app.use(bodyParsor.json);
-app.listen(3000);
+app.use(bodyParsor.json());
+
+const port = 3001;
+app.listen(port);
 
 app.get('/', (req, res, next) => {
+    console.log('requset received');
     res.send('Hello world');
 })
+
+
 
 
 
